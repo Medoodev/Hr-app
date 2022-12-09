cea6ad9224fba159b3d431ef3a38c877a4bc2569
AhmedCoder <AhmedattiaCoder@gmail.com>
2022-12-10 00:34:19 +0200

    created files

---
+++ hr_design.py
@@ -0,0 +1,156 @@
+window='''
+.QMainWindow{
+    background-color:qlineargradient(x1:0,x2:1,x3:2 ,stop:0.49 #1a4e38 ,stop: 1 #5ee8ae,stop:0.12 #2afca4 );
+    border-radius:10px;
+}
+'''
+#background-color:#f7f7f7;
+window2='''
+.QMainWindow{
+    background-color:#57efb3;
+}
+'''
+e='''
+.QLineEdit{
+    background-color:#00fffb;
+    font-size:20px;
+    border-radius:10px;
+}
+.QLineEdit:hover{
+    background-color:#01dad7;
+    border:2px solid #0eb0ae;
+    font-size:20px;
+    border-radius:10px;
+}
+'''
+e0='''
+.QLineEdit{
+    background-color:#41fe45;
+    font-size:20px;
+    border-radius:10px;
+}
+.QLineEdit:hover{
+    background-color:#02ad06;
+    border:2px solid #249727;
+    font-size:20px;
+    border-radius:10px;
+}
+'''
+e00='''
+.QLineEdit{
+    background-color:#2920ff;
+    font-size:20px;
+    border-radius:10px;
+}
+.QLineEdit:hover{
+    background-color:#0700b4;
+    border:2px solid #0a066b;
+    font-size:20px;
+    border-radius:10px;
+}
+'''
+buttons='''
+.QPushButton{
+    background-color:#5aff28;
+    font-size:20px;
+    border-radius:10px;
+}
+.QPushButton:hover{
+    background-color:#50e024;
+    border:2px solid #52ea24;
+    font-size:20px;
+    border-radius:10px;
+}
+'''
+buttons1='''
+.QPushButton{
+    background-color:#ff7b00;
+    font-size:20px;
+    border-radius:30px;
+}
+.QPushButton:hover{
+    background-color:#c66000;
+    border:2px solid #cd7e33;
+    font-size:20px;
+    border-radius:30px;
+}
+'''
+buttons2='''
+.QPushButton{
+    background-color:#ffdb0f;
+    font-size:20px;
+    border-radius:10px;
+}
+.QPushButton:hover{
+    background-color:#c2a500;
+    border:2px solid #ad9300;
+    font-size:20px;
+    border-radius:10px;
+}
+'''
+buttons3='''
+.QPushButton{
+    background-color:#1afdec;
+    font-size:20px;
+    border-radius:10px;
+}
+.QPushButton:hover{
+    background-color:#01a195;
+    border:2px solid #2d958d;
+    font-size:20px;
+    border-radius:10px;
+}
+'''
+buttons4='''
+.QPushButton{
+    background-color:#f6ff16;
+    font-size:20px;
+    border-radius:10px;
+}
+.QPushButton:hover{
+    background-color:#ccd500;
+    border:2px solid #a1a710;
+    font-size:20px;
+    border-radius:10px;
+}
+'''
+l='''
+.QLabel{
+    font-size:20px;
+    color:red;
+}
+'''
+ll='''
+.QLabel{
+    background-color:#00ffdd;
+    font-size:30px;
+}
+'''
+lll='''
+.QLabel{
+    font-size:30px;
+}
+'''
+i='''
+.QLabel{
+    border-radius:10px;
+}
+'''
+d0='''
+.QDialog{
+    background-color:#1e90ff; 
+}
+'''
+
+i0='''
+.QListWidget{
+    background-color:#4169e1 ;
+    border-radius:10px;
+    font-size:20px;
+}
+'''
+d00='''
+.QDialog{
+    background-color:#5bfa2e; 
+}
+'''
